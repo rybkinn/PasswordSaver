1441e7cf115d21ee6082acd34befd2b2006468bd
Nikita <4rybkin@gmail.com>
2021-09-04 14:30:34 +0300

    protection against the second launch of the file

--- password_saver.py
+++ password_saver.py
@@ -5,6 +5,9 @@
 import sys
 
 from PyQt5 import QtWidgets
+from PyQt5 import QtCore
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QMessageBox
 
 import py.start_window as start_window
 
@@ -16,7 +19,27 @@ class Interface(QtWidgets.QDialog, start_window.Ui_Dialog):
 
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    window = Interface()
-    window.show()
-    sys.exit(app.exec_())
+    lock_file = None
+    try:
+        app = QtWidgets.QApplication(sys.argv)
+        lock_file = QtCore.QLockFile("password_saver.lock")
+
+        if lock_file.tryLock():
+            window = Interface()
+            window.show()
+
+            sys.exit(app.exec_())
+        else:
+            icon_ps = QtGui.QIcon()
+            icon_ps.addPixmap(
+                QtGui.QPixmap(":/resource/image/key.ico"),
+                QtGui.QIcon.Normal, QtGui.QIcon.Off)
+            msg_lock = QMessageBox()
+            msg_lock.setWindowIcon(icon_ps)
+            msg_lock.setIcon(QMessageBox.Warning)
+            msg_lock.setWindowTitle("Ошибка")
+            msg_lock.setText("Программа уже запущена.")
+            msg_lock.setStandardButtons(QMessageBox.Ok)
+            msg_lock.exec_()
+    finally:
+        lock_file.unlock()
